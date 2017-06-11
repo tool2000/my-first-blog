5f63f6cf895fbba16d31b989d9e47ea28be0f170
daeyoung <shareit@hanmail.net>
2017-06-11 15:26:46 +0900

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
+from django.conf.urls import include, url
 from django.contrib import admin
-from .models import Post
+#from .models import Post
 
-admin.site.register(Post)
+#admin.site.register(Post)
 # Register your models here.
+
+urlpatterns = [
+    url('^admin/', admin.site.urls),
+    url('', include('blog.urls'))
+]
